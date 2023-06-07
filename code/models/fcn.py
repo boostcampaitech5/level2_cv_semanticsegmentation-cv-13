51abfdb456a1602f5c0deeddd7152f401a05ef3d
jusung <kimjusung2109@naver.com>
2023-06-07 03:13:14 +0000

    feat: FCN_Resnet50 모델 추가
     #1

---
+++ code/models/fcn.py
@@ -0,0 +1,14 @@
+from torch import nn 
+from torchvision import models 
+
+class FCN_Resnet50(nn.Module): 
+    def __init__(self, num_classes=29): 
+        super().__init__() 
+        self.fcn_resnet50 = models.segmentation.fcn_resnet50(pretrained=True) 
+        
+        self.fcn_resnet50.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1) 
+        
+    def forward(self, x): 
+        return self.fcn_resnet50(x) 
+    
+    
