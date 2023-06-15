6f4a2763a9849d15f77c0b6ca647a20008e7910e
jusung <kimjusung2109@naver.com>
2023-06-15 10:09:50 +0000

    feat: FCN_Resent101 모델 추가
    #15

--- code/models/fcn.py
+++ code/models/fcn.py
@@ -12,3 +12,13 @@ class FCN_Resnet50(nn.Module):
         return self.fcn_resnet50(x) 
     
     
+class FCN_Resnet101(nn.Module): 
+    def __init__(self, num_classes=29): 
+        super().__init__() 
+        self.fcn_resnet101 = models.segmentation.fcn_resnet101(pretrained=True) 
+        
+        self.fcn_resnet101.classifier[4] = nn.Conv2d(512, num_classes, kernel_size=1) 
+        
+    def forward(self, x): 
+        return self.fcn_resnet101(x)  
+    
