0095cd2e380035b4ded51f56ecc9046642549b44
jusung <kimjusung2109@naver.com>
2023-06-18 12:41:49 +0000

    feat: dice loss와 combined loss 추가
     #20

--- code/utils/loss.py
+++ code/utils/loss.py
@@ -2,10 +2,6 @@ import torch
 import torch.nn as nn 
 import torch.nn.functional as F 
 
-_criterion_entrypoints = {
-    'BCEWithLogitsLoss': nn.BCEWithLogitsLoss,
-}
-
 
 def create_criterion(criterion_name, **kwargs): 
     if criterion_name in _criterion_entrypoints: 
@@ -13,4 +9,37 @@ def create_criterion(criterion_name, **kwargs):
         criterion = create_fn(**kwargs)
     else: 
         return RuntimeError('Unknown loss (%s)' % criterion_name) 
-    return criterion 
+    return criterion  
+
+def dice_loss(pred, target, smooth = 1.):
+    pred = pred.contiguous()
+    target = target.contiguous()   
+    intersection = (pred * target).sum(dim=2).sum(dim=2)
+    loss = (1 - ((2. * intersection + smooth) / (pred.sum(dim=2).sum(dim=2) +   target.sum(dim=2).sum(dim=2) + smooth)))
+    return loss.mean() 
+
+
+class CombinedLoss(): 
+    '''
+        Binart Cross Entropy Loss + Dice Loss 
+    '''
+    def __init__(self, bce_weight = 0.5): 
+        self.bce_weight = bce_weight 
+    
+    def __call__(self, pred, target): 
+        
+        # Binary Cross Entropy Loss 
+        bce = F.binary_cross_entropy_with_logits(pred, target) 
+        # Dice Loss
+        pred = F.sigmoid(pred)
+        dice = dice_loss(pred, target) 
+        
+        loss = bce * self.bce_weight + dice * (1 - self.bce_weight)
+        
+        return loss 
+    
+
+_criterion_entrypoints = {
+    'BCEWithLogitsLoss': nn.BCEWithLogitsLoss,
+    'CustomCombinedLoss': CombinedLoss,
+}
